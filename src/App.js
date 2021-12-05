6751badc132c0fb1de5ed3f9b85455e31eecf8de
N Mouli <nbncmouli@gmail.com>
2021-12-06 03:25:46 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Footer from "./Footer/Footer";
 import Mainbar from "./Main/MainBar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div style={{ padding: "0.5em 0 0 0" }}>
       <Header />
       <Mainbar/>
+      <Footer/>
     </div>
   );
 }
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,12 +1,101 @@
-import React from 'react'
-import styled from 'styled-components';
-import { Box } from '@material-ui/core';
+import React from "react";
+import styled from "styled-components";
+import { Box, Typography } from "@material-ui/core";
+import {
+  Copyright,
+  Facebook,
+  Instagram,
+  Twitter,
+  YouTube,
+} from "@material-ui/icons";
 function Footer() {
-    return (
-        <Box>
-            
-        </Box>
-    )
+  return (
+    <BoxStyled>
+      <FooterOne>
+        <div style={{ padding: "1em 0" }}>
+          <img
+            src={
+              "https://unice.pixelstrapthemes.vercel.app/assets/images/logo/5.png"
+            }
+            alt="unice"
+          />{" "}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "500px",
+            padding: "1em 0",
+            fontFamily: "monospace",
+          }}
+        >
+          <Typography>Home</Typography>
+          <Typography>Couple</Typography>
+          <Typography>Invitation</Typography>
+          <Typography>Album</Typography>
+          <Typography>Friends</Typography>
+          <Typography>Say</Typography>
+          <Typography>Gift</Typography>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "300px",
+            padding: "1em 0",
+          }}
+        >
+          <Facebook />
+          <Instagram />
+          <Twitter />
+          <YouTube />
+        </div>
+      </FooterOne>
+      <FooterTwo>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "2em 4em",
+            width: "20%",
+          }}
+        >
+          <Typography>Privacy Policy</Typography>
+          <Typography>Terms & Conditions</Typography>
+        </div>
+        <Typography
+          style={{ display: "flex", alignItems: "center", width: "30%" }}
+        >
+          <span>Copyright </span>
+          &nbsp;
+          <Copyright />
+          &nbsp;
+          <span> 2021 Unice By Pixelstrap</span>
+        </Typography>
+      </FooterTwo>
+    </BoxStyled>
+  );
 }
 
-export default Footer
+export default Footer;
+const BoxStyled = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const FooterOne = styled(Box)`
+  background: #f5f5f8;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1em 0em;
+`;
+const FooterTwo = styled(Box)`
+  background: #000;
+  width: 100%;
+  color: #fff;
+  font-family: Poppins;
+  display: flex;
+  justify-content: space-between;
+`;
